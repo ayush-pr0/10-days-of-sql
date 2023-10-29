67e7474b1eba87548cd5b77bcc23a0f801775617
Mohan Agrawal <105565637+gituser9911@users.noreply.github.com>
2023-10-29 23:22:46 +0530

    Update BigCountries.sql
    added comments with clear code

--- Day_1-Select/BigCountries.sql
+++ Day_1-Select/BigCountries.sql
@@ -1,7 +1,11 @@
-select name, population, area
-from world
-where area >= 3000000
-union
-select name, population, area
-from world
-where population >= 25000000;
+-- Select countries with an area greater than or equal to 3,000,000
+SELECT name, population, area
+FROM world
+WHERE area >= 3000000
+
+-- Combine with countries having a population greater than or equal to 25,000,000
+UNION ALL
+
+SELECT name, population, area
+FROM world
+WHERE population >= 25000000;
