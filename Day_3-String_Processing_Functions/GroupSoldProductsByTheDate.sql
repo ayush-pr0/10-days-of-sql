dd67da5e9d60f9b1af3285b3e7dda54e3fa77f66
Ayush Khandelwal <61639552+ayush-geeks@users.noreply.github.com>
2023-10-30 17:59:44 +0530

    Update GroupSoldProductsByTheDate.sql
    update code with comment

--- Day_3-String_Processing_Functions/GroupSoldProductsByTheDate.sql
+++ Day_3-String_Processing_Functions/GroupSoldProductsByTheDate.sql
@@ -1,4 +1,7 @@
-select sell_date, count(distinct product) as num_sold, group_concat(distinct product) as products
-from activities
-group by sell_date
-order by sell_date;
+-- Select sell_date, count of distinct products sold, and a list of distinct products sold on each date
+SELECT sell_date,
+       COUNT(DISTINCT product) AS num_sold,
+       GROUP_CONCAT(DISTINCT product) AS products
+FROM activities
+GROUP BY sell_date
+ORDER BY sell_date;
