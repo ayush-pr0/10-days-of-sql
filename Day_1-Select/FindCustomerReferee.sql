7b48747b15682bcdb6ce4853981742e1e071e9e9
net-user <84893005+net-user@users.noreply.github.com>
2023-10-30 14:07:39 +0530

    Update FindCustomerReferee.sql
    comment added for clear code

--- Day_1-Select/FindCustomerReferee.sql
+++ Day_1-Select/FindCustomerReferee.sql
@@ -1,3 +1,4 @@
-select name
-from customer
-where referee_id != 2 or referee_id is NULL;
+-- Select names of customers where referee_id is not equal to 2 or is NULL
+SELECT name
+FROM customer
+WHERE referee_id <> 2 OR referee_id IS NULL;
